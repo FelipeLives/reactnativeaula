783d4b5c609db44083a72fc9e6051aa735eeb3eb
unknown <felipe_lives@hotmail.com>
2023-05-22 16:28:24 -0300

    Meu Primeiro Commit

---
+++ src/componentes/Botao.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Button} from 'react-native'
+
+export default props => {
+    function executar() {
+        console.warn("Esta executando!!! o Botao numero 1")
+    }
+    return (
+        <>
+        <Button 
+            title="botao numero 1"
+            onPress={executar}
+            />
+        <Button 
+            title="Botao numero 2"
+            onPress={function() {
+            console.warn("Esta executando o botao numero 2!!!")
+            }}
+        
+            />
+        <Button 
+            title="Botao numero 3"
+            onPress={() => {
+            console.warn("Esta executando o botao numero 3!!!")
+            }}/>
+        </>
+        
+        
+    )
+}
